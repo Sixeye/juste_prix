43dbb0b10d19714430bde41ad4ded8ef7296a7d7
System Administrator <root@Mac-mini-de-Seye.local>
2019-11-14 09:11:39 +0100

    start

---
+++ le_juste_prix/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << 5;
+    std::cout << std::endl ;
+    // Commentaire
+    /*
+     Commentaire plus long
+     */
+    std::cout << "Bienvenue au prix juste";
+    std::cout << std::endl ;
+}
